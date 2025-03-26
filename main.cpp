6819a8f2f7dc0cbcdaee7447e79dbbc4087631f6
ANDRAS SZABO <szabo.andrew@yahoo.com>
2025-03-26 12:01:07 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "FIFO.h"
+using namespace std;
+int main()
+{
+	int size = 5;
+	FIFO p1(size);
+	for (int i = 0; i < size; i++)
+	{
+		p1.Put(i);
+	}
+	p1.printDataElements();
+}
